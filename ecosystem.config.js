2c688d002cdbb70c117acd5300a3e0252213f8b1
niah zheng <89195495@qq.com>
2025-06-01 10:54:28 +0800

    22

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   apps: [{
     name: 'watson-stt-stream-connector',
-    script: './app.js',
+    script: 'app.js',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    error_file: 'log/pm2_error.log',
+    out_file: 'log/pm2_out.log',
+    merge_logs: true,
     env: {
       NODE_ENV: 'production',
       DEFAULT_SERVER_LISTEN_PORT: process.env.PORT || 80, // Azure 会提供 PORT 环境变量
